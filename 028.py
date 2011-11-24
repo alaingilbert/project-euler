21a26ccaf2b91bf08c892d5fba1e4417147138c5
alaingilbert <alain.gilbert.15@gmail.com>
2011-11-24 01:43:52 -0500

    Optimization

--- 028.py
+++ 028.py
@@ -1,13 +1,5 @@
-n = 1
-step = 1
-sum = 0
-l = 1001
-i = 1
-while i < l ** 2 + 1:
-   if i == n ** 2:
-      n += 2
-      step = n - 1
-   sum += i
-   i += step
-
-print sum
+i = n = s = 1
+while i < 1001 ** 2 + 1:
+   if i == n ** 2: n += 2
+   s, i = s + i, i + n - 1
+print s - 1
