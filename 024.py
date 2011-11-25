5db68b636c35d3224124b2cddd4aae78fc112fba
alaingilbert <alain.gilbert.15@gmail.com>
2011-11-25 10:02:04 -0500

    Trivial problem

--- 024.py
+++ 024.py
@@ -2,18 +2,12 @@ a = [1]
 for i in range(1, 9): a.append(a[i-1] * (i+1))
 a.reverse()
 
-s = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ]
-f = []
-n = 999999
+s, n = ['0','1','2','3','4','5','6','7','8','9'], 999999
 c = it = 0
 while n > 0:
-   if n >= a[it]:
-      n -= a[it]
-      c += 1
+   if n >= a[it]: n, c = n-a[it], c+1
    if n < a[it]:
-      f.append(s.pop(c))
-      it += 1
-      c = 0
-f.append(s.pop(c))
+      s.insert(it, s.pop(c + it))
+      it, c = it+1, 0
 
-print ''.join(f)
+print ''.join(s)
