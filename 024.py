5459de097fa37e55b54db5d99276357979be2d4f
alaingilbert <alain.gilbert.15@gmail.com>
2011-11-25 10:19:49 -0500

    trivial

--- 024.py
+++ 024.py
@@ -1,9 +1,8 @@
-a, s, n = [1], ['0','1','2','3','4','5','6','7','8','9'], 999999
-c = it = 0
-for i in range(2, 10): a.insert(0, a[0] * i)
+a, s, n, c, i = [1], ['0','1','2','3','4','5','6','7','8','9'], 999999, 0, 0
+for x in range(2, 10): a.insert(0, a[0] * x)
 while n > 0:
-   if n >= a[it]: n, c = n-a[it], c+1
-   if n < a[it]:
-      s.insert(it, s.pop(c + it))
-      it, c = it+1, 0
+   if n >= a[i]: n, c = n-a[i], c+1
+   if n < a[i]:
+      s.insert(i, s.pop(c + i))
+      i, c = i+1, 0
 print ''.join(s)
