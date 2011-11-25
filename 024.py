ce336232a7d549deb314ec082d8942315c1aacd3
alaingilbert <alain.gilbert.15@gmail.com>
2011-11-24 19:54:28 -0500

    still trivial

--- 024.py
+++ 024.py
@@ -2,7 +2,7 @@ a = [1]
 for i in range(1, 9): a.append(a[i-1] * (i+1))
 a.reverse()
 
-s = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+s = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ]
 f = []
 n = 999999
 c = it = 0
@@ -16,4 +16,4 @@ while n > 0:
       c = 0
 f.append(s.pop(c))
 
-print ''.join(str(x) for x in f)
+print ''.join(f)
